f185a78c05d06c9c304230a45d36cce228250b75
mthsps <dmatheusps@gmail.com>
2022-11-19 20:35:49 -0300

    Added data access layer to base project

---
+++ chatard/DataAccess/Context.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+
+
+namespace chatard.DataAccess
+{
+    public class Context : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConfigurationManager.ConnectionStrings["ChatardDatabase"].ConnectionString);
+        }
+        
+        public Context(DbContextOptions<Context> options) : base(options) { 
+        }
+
+
+    }
+}
