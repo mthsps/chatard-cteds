4add86581f08bffd27b03d2de0b0b27745d40162
BnyioM <brunomaia.duarte@gmail.com>
2022-12-09 19:41:41 -0300

    ViewModels

---
+++ chatard/ViewModels/LoginViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chatard.ViewModels
+{
+    public class LoginViewModel : ViewModelBase
+    {
+        private string _username;
+        private string _password;
+        private string _error;
+        bool _isVisible = true;
+
+        public string Username 
+        { 
+            get
+            {
+                return _username;
+            }  
+            set => _username = value; 
+        }
+        public string Password { get => _password; set => _password = value; }
+        public string Error { get => _error; set => _error = value; }
+        public bool IsVisible { get => _isVisible; set => _isVisible = value; }
+    }
+}
