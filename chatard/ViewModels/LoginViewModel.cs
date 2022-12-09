6c4e8b31d3a89fc30df69e693eae0f42e4e821c9
BnyioM <brunomaia.duarte@gmail.com>
2022-12-09 20:36:50 -0300

    authenticatio branch

--- chatard/ViewModels/LoginViewModel.cs
+++ chatard/ViewModels/LoginViewModel.cs
@@ -18,11 +18,44 @@ namespace chatard.ViewModels
             get
             {
                 return _username;
-            }  
-            set => _username = value; 
+            }
+            set
+            {
+                _username = value;
+            }
         }
-        public string Password { get => _password; set => _password = value; }
-        public string Error { get => _error; set => _error = value; }
-        public bool IsVisible { get => _isVisible; set => _isVisible = value; }
+        public string Password 
+        {
+            get 
+            {
+                return _password;
+            }
+            set
+            {
+                _password = value;
+            } 
+        }
+        public string Error 
+        {
+            get
+            {
+                return _error;
+            }
+            set
+            {
+                _error = value;
+            } 
+        }
+        public bool IsVisible {
+            get
+            {
+                return _isVisible;
+            }
+            set
+            {
+                _isVisible = value;
+                OnPropertyChanged(nameof(IsVisible));
+            }
+        }
     }
 }
--- chatard/ViewModels/ViewModelBase.cs
+++ chatard/ViewModels/ViewModelBase.cs
@@ -4,9 +4,14 @@ using System.Runtime.CompilerServices;
 
 namespace chatard.ViewModels
 {
-    public abstract class ViewModelBase : INotifyPropertyChanged
+    public class ViewModelBase : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+
+        }
 
         public Context context = new Context();
         public ViewModelBase()
